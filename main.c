9d8f0c6e40a38acee745250a8e047a0b2cbf7364
Hubert Bańkowski <05hubert05@gmail.com>
2025-12-30 22:36:34 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Implant {
+    char owner_id[50];
+    char name[100];
+    char manufacturer[100];
+    int risk_level;
+    float energy_demand;
+    char status[20];
+    struct Implant* next; // Wskaźnik na kolejny element listy
+} Implant;
+int main() {
+}
