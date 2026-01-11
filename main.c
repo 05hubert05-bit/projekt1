ca0fc021da32de47b583d794664dc87aaf9821d5
Hubert Bańkowski <05hubert05@gmail.com>
2026-01-11 23:52:56 +0100

    menu

--- main.c
+++ main.c
@@ -11,5 +11,29 @@ typedef struct Implant {
     char status[20];
 
 } Implant;
+int showMenu()
+{
+    int wybor;
+    printf("\n===MENU===\n");
+    printf("1)dodaj\n");
+    printf("2)usun\n");
+    printf("3)modifikuj\n");
+    printf("4)sortuj\n");
+    printf("5)wyszukaj\n");
+    printf("6)\n");
+         if(scanf("%d",&wybor)!=1 || wybor<1 || wybor>7)
+         {
+             printf("Wprowadz poprawna wartosc\n");
+          while ((getchar()) != '\n');
+         }
+         else{
+            return wybor;
+         }
+}
 int main() {
+    int menu=1;
+    int wybor;
+       while(menu==1){
+         wybor=showMenu();
+       }
 }
