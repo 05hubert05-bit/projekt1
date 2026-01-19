3c0d6cc9c2ffa5f8467f0304a89899958fe5a9cd
05hubert05-bit <05hubertt05@gmail.com>
2026-01-19 23:58:07 +0100

    edycja i walidacja

--- Documents/projekt1/main.c
+++ Documents/projekt1/main.c
@@ -33,6 +33,10 @@ int showMenu()
             return wybor;
          }
 }
+void wyczysc_bufor() {
+    int c;
+    while ((c = getchar()) != '\n');
+}
 Implant* Nowyimplant(char* id,char* nazwa,char* producent,int ryzyko,float energia,char* status)
 {
     Implant* nowy = calloc(1, sizeof(Implant));
@@ -50,13 +54,12 @@ void Dodaj_implant(Implant** head) {
     char id[50], nazwa[100], producent[100], status[20];
     int ryzyko; float energia;
 
-    printf("ID Wlasciciela: "); scanf("%s", id);
-    printf("Nazwa implantu: "); scanf("%s", nazwa);
-    printf("Producent: "); scanf("%s", producent);
-    printf("Ryzyko (0-10): "); scanf("%d", &ryzyko);
-    printf("Energia: "); scanf("%f", &energia);
-    printf("Status: "); scanf("%s", status);
-
+    printf("ID Wlasciciela: "); scanf("%49s", id);wyczysc_bufor();
+    printf("Nazwa implantu: "); scanf("%99s", nazwa);wyczysc_bufor();
+    printf("Producent: "); scanf("%99s", producent);wyczysc_bufor();
+    printf("Ryzyko (0-10): "); while(scanf("%d", &ryzyko) != 1 || ryzyko<0 || ryzyko>10){printf("Blad: Nieprawidlowe ryzyko\n Zakres(0-10)\n"); wyczysc_bufor();}
+    printf("Energia: "); while(scanf("%f", &energia) != 1){printf("Blad:wpisz liczbe\n"); wyczysc_bufor();}
+    printf("Status:(legalny/szara/nielegalny)"); while(scanf("%19s", status) != 1 || (strcmp(status,"legalny")!=0 && strcmp(status,"szara")!=0 && strcmp(status,"nielegalny")!=0)){printf("Blad: Nieprawidlowy status\n Opcje(legalny/szara/nielegalny)\n"); wyczysc_bufor();}
     Implant* nowy = Nowyimplant(id, nazwa, producent, ryzyko, energia, status);
     nowy->next = *head; 
     *head = nowy;
@@ -67,12 +70,12 @@ void wyswietl(Implant* head) {
 
     while (p != NULL) {
      
-        printf("%s\n ", p->id);
-        printf("%s\n ", p->nazwa);
-           printf("%s\n", p->producent);
-             printf("%d\n", p->ryzyko);
-            printf("%.2f\n", p->energia);
-             printf("%s\n", p->status);
+        printf("ID: %s\n ", p->id);
+        printf("Nazwa: %s\n ", p->nazwa);
+           printf("Producent: %s\n", p->producent);
+             printf("Ryzyko: %d\n", p->ryzyko);
+            printf("Energia: %.2f\n", p->energia);
+             printf("Status: %s\n", p->status);
       
      
 
@@ -80,6 +83,56 @@ void wyswietl(Implant* head) {
     }
 
 }
+void edytuj_implant(Implant* head) {
+    char tym_id[50];
+    char tym_nazwa[100];
+
+    printf("\n--- MODYFIKACJA DANYCH IMPLANTU ---\n");
+    
+  
+    printf("Podaj ID właściciela: ");
+    scanf("%49s", tym_id);wyczysc_bufor();
+    printf("Podaj nazwę implantu: ");
+    scanf("%99s", tym_nazwa);wyczysc_bufor();
+
+
+    Implant* current = head;
+    while (current != NULL) {
+     
+        if (strcmp(current->id, tym_id) == 0 && strcmp(current->nazwa, tym_nazwa) == 0) {
+            
+          
+            printf("\nZnaleziono rekord. Obecne dane:\n");
+            printf("Producent: %s | Ryzyko: %d | Energia: %.2f | Status: %s\n",
+                   current->producent, current->ryzyko, 
+                   current->energia, current->status);
+
+            printf("\n--- WPROWADŹ NOWE DANE ---\n");
+            
+            
+            
+            printf("Nowy producent: ");
+            scanf("%99s", current->producent);wyczysc_bufor();
+
+            printf("Nowy poziom ryzyka (0-10): ");
+            scanf("%d", &current->ryzyko);wyczysc_bufor();
+
+            printf("Nowe zapotrzebowanie energetyczne: ");
+            scanf("%f", &current->energia);wyczysc_bufor();
+
+            printf("Nowy status (legalny/szara/nielegalny): ");
+            scanf("%19s", current->status);wyczysc_bufor();
+
+            printf("SUKCES: Dane implantu zaktualizowane.\n");
+            return; 
+        }
+        current = current->next;
+    }
+
+    
+    printf("BŁĄD: Nie znaleziono implantu o podanych parametrach.\n");
+}
+
 int main() {
     Implant* head = NULL;
     int menu=1; 
@@ -96,7 +149,7 @@ int main() {
    break;
      }
  case 3:{
-
+    edytuj_implant(head);
 break;
 }
  case 4:{
